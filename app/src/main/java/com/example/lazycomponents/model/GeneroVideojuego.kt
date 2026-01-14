50f9e23e3e6815b2d25727c9a21245f527ea6444
javier.grima@gracia.lasalle.cat <javier.grima@gracia.lasalle.cat>
2026-01-14 18:29:00 +0100

    Crear carpetas y crear data class GeneroVideojuego

---
+++ app/src/main/java/com/example/lazycomponents/model/GeneroVideojuego.kt
@@ -0,0 +1,8 @@
+package com.example.lazycomponents.model
+
+data class GeneroVideojuego(
+    val nombreJuego: String,
+    val genero: String,
+    val imagenUrl: String,
+    val descripcion: String
+)
